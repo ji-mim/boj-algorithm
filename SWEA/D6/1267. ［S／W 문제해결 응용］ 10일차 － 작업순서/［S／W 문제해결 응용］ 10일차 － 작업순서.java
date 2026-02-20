1b27c49cf4d63c9a70c7f756e713a145959e051d
jimin Choi <138632648+ji-mim@users.noreply.github.com>
2026-02-20 11:51:16 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 106 ms, Memory: 28,672 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
@@ -3,66 +3,63 @@ import java.util.*;
 
 public class Solution {
 	
-	static int[][] graph;
+	static int V,E;
+	static ArrayList<Integer>[] graph;
 	static int[] indegree;
-	static int V,E ;
 	static ArrayDeque<Integer> que = new ArrayDeque<>();
 	static StringBuilder sb;
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		for (int tc = 1 ; tc <= 10 ; tc ++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			V = Integer.parseInt(st.nextToken());
 			E = Integer.parseInt(st.nextToken());
-			que.clear();
-			graph = new int[V + 1][V + 1];
-			indegree = new int[V + 1];
 			sb = new StringBuilder();
+			indegree = new int[V + 1];
+			que.clear();
+			
 			sb.append("#").append(tc).append(" ");
 			
+			graph = new ArrayList[V + 1];
+			
+			for (int i = 0 ; i <= V ; i ++) {
+				graph[i] = new ArrayList<>();
+			}
+			
 			st = new StringTokenizer(br.readLine());
-			for (int i = 0 ; i < E  ; i ++) {
-				int from = Integer.parseInt(st.nextToken());
+			
+			for (int i = 0 ; i < E ; i ++) {
 				int to = Integer.parseInt(st.nextToken());
-				graph[from][to] = 1;
-				indegree[to] += 1;
+				int from = Integer.parseInt(st.nextToken());
+				graph[to].add(from);
+				indegree[from] ++;
 			}
 			
-			for (int j = 1; j <= V ; j ++) {
-				boolean isStart = true;
-				for (int i = 1; i <= V ; i ++) {
-					if(graph[i][j] == 1) isStart = false;
-				}
-				
-				if(isStart) {
-					que.add(j);
-				}
+			for (int i = 1 ; i <= V ; i ++) {
+				if(indegree[i] == 0) que.add(i);
 			}
-			
 			
 			bfs();
+			
 			System.out.println(sb);
 			
 			
 		}
+		
 	}
 	
 	public static void bfs() {
-		
 		while(!que.isEmpty()) {
-			int curr = que.poll();
-			sb.append(curr).append(" ");
+			int cur = que.poll();
+			sb.append(cur).append(" ");
 			
-			for (int i = 1 ; i <= V ; i ++) {
-				if(graph[curr][i] == 1 ) {
-					if(--indegree[i] == 0) {
-						que.offer(i);
-					}
-				}
+			for (int i = 0 ; i < graph[cur].size(); i ++) {
+				if(--indegree[graph[cur].get(i)] == 0) que.offer(graph[cur].get(i));
 			}
 		}
+		
 	}
 
 }
