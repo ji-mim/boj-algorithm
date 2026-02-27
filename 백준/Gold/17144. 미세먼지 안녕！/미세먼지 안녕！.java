fff70a35d029d9daca66c02ae2281d4191b33146
jimin Choi <138632648+ji-mim@users.noreply.github.com>
2026-02-27 17:23:29 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 284 ms, Memory: 32264 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -1,197 +1,116 @@
-
-import java.io.*;
-import java.util.*;
-
-class Dirst{
-	int x, y, size;
-	
-	public Dirst(int x, int y, int size) {
-		this.x = x;
-		this.y = y;
-		this.size = size;
-	}
-}
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-
-	static int[] up, down;
-	static int N, M, T;
-	static int [][] arr, move, spread;  
-	static ArrayDeque<Dirst> dirstQ = new ArrayDeque<>();
-	static int[] dx = {-1,1,0,0};
-	static int[] dy = {0,0,-1,1};
 	
-	public static void main (String args[]) throws IOException{
+	public static int ROW, COL, T, MAP[][], robot[];
+	public static int[] dr = {0, 0, 1, -1, 0};
+	public static int[] dc = {1, -1, 0, 0, 0};
+	
+	
+	public static void main(String[] args) throws Exception {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		ROW = Integer.parseInt(st.nextToken());
+		COL = Integer.parseInt(st.nextToken());
 		T = Integer.parseInt(st.nextToken());
-		
-		arr = new int[N][M];
-		move = new int[N][M];
-		spread = new int[N][M];
-		up = new int[2];
-		down = new int[2];
-		
-		
-		for (int i = 0 ; i < N ; i ++) {
+		robot = new int[2];
+		MAP = new int[ROW][COL];
+		for(int i = 0; i < ROW; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0 ; j < M ; j ++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-				if (arr[i][j] == -1 && up[0] == 0) { // 여기서 실수 
-					up[0] = i;
-					up[1] = j;
-					down[0] = i + 1;
-					down[1] = j;
-				}
-				if(arr[i][j] > 0) {
-					dirstQ.add(new Dirst(i, j, arr[i][j]));
+			for(int j = 0; j < COL; j++) {
+				MAP[i][j] = Integer.parseInt(st.nextToken());
+				if(MAP[i][j] == -1) {
+					robot[0] = i-1;
+					robot[1] = i;
 				}
 			}
 		}
-		
-		for (int t = 0 ; t < T ; t ++) {
-			initArrs(); // move,spread 초기화
-			
-			spreadDirst(); // dirstList의 먼지 전파
-			
-			blow(); // 공기청정기 동작 바람 돌리기 
-			
-			//배열 동기화, que에 집어넣기 
-			sync();
+		for(int t = 1; t <= T; t++) {
+			spread();
+			move();
 		}
-		
-		int ans = getAns();
-		System.out.println(ans);
+		int sum = 0;
+		for(int i = 0; i < ROW; i++) {
+			for(int j = 0; j < COL; j++) {
+				if(MAP[i][j] == -1) continue;
+				sum += MAP[i][j];
+			}
+		}
+		System.out.println(sum);
 		
 	}
-	
-	public static void spreadDirst() {
-		while(!dirstQ.isEmpty()) {
-			Dirst curr = dirstQ.poll();
-			int x = curr.x;
-			int y = curr.y;
-			int size = curr.size; 
-			int spreadSize = (int) curr.size / 5; 
-			
-			for (int k = 0 ; k < 4 ; k ++) {
-				int nx = x + dx[k];
-				int ny = y + dy[k];
+
+
+	private static void move() {
+        int up = robot[0];
+        int down = robot[1];
+
+        // 위쪽: 반시계
+        // 1) 왼쪽 세로 (위로)
+        for (int i = up - 1; i > 0; i--) MAP[i][0] = MAP[i - 1][0];
+        // 2) 위 가로 (왼쪽으로)
+        for (int j = 0; j < COL - 1; j++) MAP[0][j] = MAP[0][j + 1];
+        // 3) 오른쪽 세로 (아래로)
+        for (int i = 0; i < up; i++) MAP[i][COL - 1] = MAP[i + 1][COL - 1];
+        // 4) 아래 가로 (오른쪽으로)
+        for (int j = COL - 1; j > 1; j--) MAP[up][j] = MAP[up][j - 1];
+        MAP[up][1] = 0; // 청정기에서 나온 바람은 0
+
+        // 아래쪽: 시계
+        // 1) 왼쪽 세로 (아래로)
+        for (int i = down + 1; i < ROW - 1; i++) MAP[i][0] = MAP[i + 1][0];
+        // 2) 아래 가로 (왼쪽으로)
+        for (int j = 0; j < COL - 1; j++) MAP[ROW - 1][j] = MAP[ROW - 1][j + 1];
+        // 3) 오른쪽 세로 (위로)
+        for (int i = ROW - 1; i > down; i--) MAP[i][COL - 1] = MAP[i - 1][COL - 1];
+        // 4) 위 가로 (오른쪽으로)
+        for (int j = COL - 1; j > 1; j--) MAP[down][j] = MAP[down][j - 1];
+        MAP[down][1] = 0;
+
+        // 공기청정기 위치 유지
+        MAP[up][0] = -1;
+        MAP[down][0] = -1;
+	}
+
+
+	private static void spread() {
+
+		int[][] temp = new int[ROW][COL];
+        // 공기청정기 위치 고정
+        temp[robot[0]][0] = -1;
+        temp[robot[1]][0] = -1;
+        
+		for(int i = 0; i < ROW; i++) {
+			for(int j = 0; j < COL; j++) {
+				if(MAP[i][j] <= 0) continue;
+
+				int dust = MAP[i][j];
+				int amount = dust / 5;
+				int cnt = 0;
 				
-				if(canSpread(nx,ny)) { // 배열 안에 있고, 공기청정기와 만나지 않을 때 
-					spread[nx][ny] += spreadSize;
-					size -= spreadSize;
+				
+				for(int d = 0; d < 4; d++) {
+					int r = dr[d] + i;
+					int c = dc[d] + j;
+					if(r < 0 || r >= ROW || c < 0 || c >= COL) {
+						continue;
+					}
+					if(MAP[r][c] == -1) {
+						continue;
+					}
+					temp[r][c] += amount;
+					cnt++;
 				}
-			}
-			spread[x][y] += size;
-		}
-		
-		// 이부분 빠트림 
-		for (int i = 0 ; i < N ; i ++) {
-			for (int j = 0 ; j < M ; j ++) { 
-				move[i][j] = spread[i][j];
+				
+				temp[i][j] += dust - amount * cnt;
 			}
 		}
-	}
-	
-	public static boolean canSpread(int x, int y) {
-		return ((0<= x && x < N && 0<=y && y < M) && arr[x][y] != -1);
-	}
-	
-	public static void blow() {
-		// 위쪽 
-		blowUpArea();
-		//아래쪽 
-		blowDownArea();
-	}
-	
-	public static void blowDownArea() {
-		int x = down[0];
-		int y = down[1];
 		
-		//왼쪽 
-		for (int i = x ; i < N - 1 ; i ++ ) {
-			move[i][0] = spread[i + 1][0];
-		}
+		MAP = temp;
 		
-		//아래쪽
-		for(int i = 0 ; i < M - 1 ; i ++) {
-			move[N - 1][i] = spread[N - 1][i + 1];
-		}
-		
-		//오른쪽
-		for(int i = N - 1 ; i > x ; i --) {
-			move[i][M - 1] = spread[i - 1][M - 1];
-		}
-		
-		//위쪽
-		for(int i = M - 1 ; i > 0 ; i --) {
-			move[x][i] = spread[x][i - 1];
-		}
-		
-		move[x][y + 1] = 0;
-		move[x][y] = -1;
-	}
-	
-	public static void blowUpArea() {
-		int x = up[0];
-		int y = up[1];
-		
-		//왼쪽  
-		for (int i = x ; i > 0 ; i -- ) {
-			move[i][0] = spread[i - 1][0];
-		}
-		
-		//위쪽
-		for(int i = 0 ; i < M - 1 ; i ++) {
-			move[0][i] = spread[0][i + 1];
-		}
-		
-		//오른쪽 여기 실수
-		for(int i = 0 ; i < x ; i ++) {
-			move[i][M-1] = spread[i + 1][M-1];
-		}
-		
-		//아래쪽
-		for(int i = M - 1 ; i > 0 ; i --) {
-			move[x][i] = spread[x][i - 1];
-		}
-		
-		move[x][y + 1] = 0;
-		move[x][y] = -1;
-	}
-	
-	public static void sync() {
-		for (int i = 0 ; i < N ; i ++) {
-			for (int j = 0 ; j < M ; j ++) { 
-				arr[i][j] = move[i][j];
-				if (arr[i][j] > 0) {
-					dirstQ.add(new Dirst(i, j, arr[i][j]));
-				}
-			}
-		}
-	}
-	
-	public static int getAns() {
-		int cnt = 0 ;
-		for (int i = 0 ; i < N ; i ++) {
-			for (int j = 0 ; j < M ; j ++) { 
-				if (arr[i][j] > 0) {
-					cnt += arr[i][j];
-				}
-			}
-		}
-		return cnt;
-	}
-	
-	
-	
-	public static void initArrs() {
-		for (int i = 0 ; i < N ; i ++) {
-			for (int j = 0 ; j < M ; j ++) { 
-				move[i][j] = spread[i][j] = 0;
-			}
-		}
 	}
 }
