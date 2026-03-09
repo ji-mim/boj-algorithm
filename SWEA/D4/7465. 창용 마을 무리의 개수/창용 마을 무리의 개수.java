0f0efc55b7c83988aaea9d6869a10fb2c155a882
jimin Choi <138632648+ji-mim@users.noreply.github.com>
2026-03-09 14:47:39 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 98 ms, Memory: 28,032 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,56 +1,56 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Solution {
-
-    static int T, N, M ;
-    static int ans ;
-    static int [][] graph;
-    static boolean [] visited;
-    
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        T = Integer.parseInt(br.readLine());
-
-        for (int tc = 1 ; tc <= T ; tc ++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-            ans = 0 ;
-
-            graph = new int[N+1][N+1];
-            visited = new boolean[N + 1];
-
-            for (int i = 0 ; i < M ; i ++){
-                st = new StringTokenizer(br.readLine());
-                int from = Integer.parseInt(st.nextToken());
-                int to = Integer.parseInt(st.nextToken());
-                graph[from][to] = 1;
-                graph[to][from] = 1;
-            }
-
-            for (int i = 1 ; i <= N ; i ++){
-                if(!visited[i]){
-                    dfs(i);
-                    ans ++;
-                }
-            }
-
-            System.out.println("#" + tc + " " + ans);
-        }
-
-    }
-    
-    public static void dfs(int curr){
-        visited[curr] = true;
-
-        for (int i = 1 ; i <= N ; i ++){
-            if(!visited[i] && graph[curr][i] == 1){
-                dfs(i);
-            }
-        }
-    }
-
-
+	
+	static int N, M;
+	static int[] parents;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int tc = 1 ; tc <= T ; tc ++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			makeSets();
+			
+			while(M -- > 0) {
+				st = new StringTokenizer(br.readLine());
+				int from = Integer.parseInt(st.nextToken());
+				int to = Integer.parseInt(st.nextToken());
+				union(from, to);
+			}
+			
+			boolean[] visited = new boolean[N + 1];
+			int ans = 0;
+			for (int i = 1 ; i <= N ; i ++) {
+				if(!visited[findSet(i)]) {
+					ans ++;
+					visited[findSet(i)] = true;
+				}
+			}
+			
+			System.out.println("#" + tc + " " + ans);
+		}
+		
+	}
+	
+	public static void makeSets() {
+		parents = new int[N + 1];
+		for (int i = 1 ; i <= N ; i ++) {
+			parents[i] = i ;
+		}
+	}
+	
+	public static int findSet(int node) {
+		if(parents[node] == node) return node;
+		return parents[node] = findSet(parents[node]);
+	}
+	
+	public static void union(int from, int to) {
+		if(findSet(from) == findSet(to)) return;
+		parents[findSet(to)] = findSet(from);
+	}
 }
