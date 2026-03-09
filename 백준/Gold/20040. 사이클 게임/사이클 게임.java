742124a657384a41e22129d073f474b42b4d16ff
jimin Choi <138632648+ji-mim@users.noreply.github.com>
2026-03-09 14:11:32 +0900

    [Gold IV] Title: 사이클 게임, Time: 588 ms, Memory: 127888 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.java
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -25,23 +25,17 @@ public class Main {
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
 			
-			
-			if(findRoot(from) == findRoot(to)) {
+			int rootFrom = findRoot(from);
+			int rootTo = findRoot(to);
+			if(rootFrom == rootTo) {
 				ans = i;
 				break;
 			}
 			
-			union(from, to);
-//			parents[to] = from;
+			parents[rootTo] = rootFrom;
 		}
 		
 		System.out.println(ans);
-	}
-	
-	public static boolean union(int a, int b) {
-		if(findRoot(a) == findRoot(b)) return false;
-		parents[findRoot(b)] = findRoot(a);
-		return true;
 	}
 	
 	public static int findRoot(int node) {
