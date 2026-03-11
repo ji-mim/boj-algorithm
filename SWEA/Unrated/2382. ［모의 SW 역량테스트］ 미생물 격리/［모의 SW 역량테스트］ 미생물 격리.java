b1b82a777bebccc5164f633e46d4f9cf865fa4cc
jimin Choi <138632648+ji-mim@users.noreply.github.com>
2026-03-11 16:00:36 +0900

    [Unrated] Title: [모의 SW 역량테스트] 미생물 격리, Time: 1,200 ms, Memory: 36,396 KB -BaekjoonHub

--- SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
+++ SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
@@ -3,22 +3,27 @@ import java.util.*;
 
 public class Solution {
 	
-	static class Micro{
-		int r, c, cnt, dir, total;
+	static class Micro implements Comparable<Micro>{
+		int r, c, cnt, dir;
 
 		public Micro(int r, int c, int cnt, int dir) {
 			super();
 			this.r = r;
 			this.c = c;
-			this.total = this.cnt = cnt;
+			this.cnt = cnt;
 			this.dir = dir;
+		}
+		
+		@Override
+		public int compareTo(Micro m) {
+			return Integer.compare(m.cnt, this.cnt);
 		}
 	}
 	
 	static int N, M, K;
 	static int[] dr = {0,-1,1,0,0}; //상:1, 하:1, 좌:3, 우:4
 	static int[] dc = {0,0,0,-1,1}; 
-	static Micro[] list ; // 미생물 군집 리스트 
+	static PriorityQueue<Micro> pq ; // 미생물 군집 리스트 
 	static Micro[][] map; // 미생물의 이동을 기록(병합 관리를 위해)
 	
 	public static void main(String[] args) throws IOException {
@@ -30,17 +35,17 @@ public class Solution {
 			M = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
 			
-			list = new Micro[K];
+			pq = new PriorityQueue<>();
 			map = new Micro[N][N];
 			
 			for (int i = 0 ; i < K ; i ++) {
 				st = new StringTokenizer(br.readLine());
-				list[i] = new Micro(
+				pq.add(new Micro(
 						Integer.parseInt(st.nextToken()), 
 						Integer.parseInt(st.nextToken()), 
 						Integer.parseInt(st.nextToken()), 
 						Integer.parseInt(st.nextToken())
-						);  
+						));  
 			}
 			
 			System.out.println("#" + tc + " " + solve());
@@ -53,14 +58,15 @@ public class Solution {
 		
 		// M 시간 동안 처리 
 		while(time -- > 0) {
-			for(Micro cur : list) {
-				if(cur.cnt == 0) continue; // 소멸된 군집이면 스킵
+			while(!pq.isEmpty()) {
+				Micro cur = pq.poll();
+				
 				// 군집 다음 좌표로 좌표 update,
 				nr = cur.r += dr[cur.dir];
 				nc = cur.c += dc[cur.dir];
 				// 가장자리 셀인 경우는 크기 1/2, 방향 반대로
 				if(nr == 0 || nr == N - 1 || nc == 0 || nc == N - 1) {
-					cur.total = cur.cnt /= 2;
+					cur.cnt /= 2;
 					if(cur.cnt == 0) continue;
 					if(cur.dir % 2 == 1) cur.dir++;
 					else cur.dir--;
@@ -69,15 +75,8 @@ public class Solution {
 				// 업데이트된 좌표로 이동하며 병합
 				if(map[nr][nc] == null) { // 이동하는 위치에 자신이 처음 도착하는 것
 					map[nr][nc] = cur;
-				}else { // 이동하는 위치에 먼저 도착한 군집이 있다면 군집의 크기를 비교해서 병합 
-					if(map[nr][nc].cnt > cur.cnt) {
-						map[nr][nc].total += cur.cnt;
-						cur.cnt = 0; // 현재 군집은 흡수되었으므로 미생물 수는 0으로 (소멸 상태로)
-					}else { //먼저 도착한 군집보다 현재 군집이 더 크다면 자신이 흡수함 
-						cur.total += map[nr][nc].total;
-						map[nr][nc].cnt = 0; // 군집소멸
-						map[nr][nc] = cur;
-					}
+				}else { // 이동하는 위치에 먼저 도착한 군집이 있다면 나보다 크니까 흡수당함
+					map[nr][nc].cnt += cur.cnt;
 				}
 			}
 			remainCnt = reset();
@@ -87,15 +86,16 @@ public class Solution {
 	
 	private static int reset() {
 		int result = 0;
-		for(Micro cur : list) {
-			if(cur.cnt == 0) continue;
-			
-			if(map[cur.r][cur.c] == cur) {
-				map[cur.r][cur.c] = null;
-				cur.cnt = cur.total;
+		for (int r = 0 ; r < N ; r ++) {
+			for (int c = 0 ; c < N ; c ++) {
+				if(map[r][c] == null) continue;
+				
+				result += map[r][c].cnt;
+				pq.offer(map[r][c]);
+				map[r][c] = null;
 			}
-			result += cur.cnt;
 		}
+		
 		return result;
 	}
 	
