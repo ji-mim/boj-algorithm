64751f547c793b5967673a1412f385f35fcf531d
jimin Choi <138632648+ji-mim@users.noreply.github.com>
2026-03-11 13:46:06 +0900

    [Gold II] Title: 물대기, Time: 232 ms, Memory: 23560 KB -BaekjoonHub

---
+++ 백준/Gold/1368. 물대기/물대기.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static class Node implements Comparable<Node>{
+		int vetex, weight;
+		
+		public Node(int vertex, int weight) {
+			this.vetex = vertex;
+			this.weight = weight;
+		}
+		
+		@Override
+		public int compareTo(Node n) {
+			return Integer.compare(this.weight, n.weight);
+		}
+	}
+
+	public static void main(String[] args) throws IOException{
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] digCost = new int[N];
+		int[] minEdge = new int[N];
+		
+		for (int i = 0 ; i < N ; i ++) {
+			minEdge[i] = Integer.parseInt(br.readLine());
+		}
+		
+//		Arrays.fill(minEdge, Integer.MAX_VALUE);
+		
+		int[][] graph = new int[N][N];
+		
+		for (int i = 0 ; i < N ; i ++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0 ; j < N ; j ++) {
+				graph[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		boolean[] visited = new boolean[N];
+		
+		int result = 0;
+		
+//		minEdge[0] = 0;
+		
+		for (int c = 0 ; c < N ; c ++) {
+			int minVertex = -1 ; 
+			int minWeight = Integer.MAX_VALUE; 
+			
+			for (int i = 0 ; i < N ; i ++) {
+				if(!visited[i] && minWeight > minEdge[i]) {
+					minVertex = i;
+					minWeight = minEdge[i];
+				}
+				
+			}
+			if(minVertex == -1) break;
+			visited[minVertex] = true;
+					
+			for (int i = 0 ; i < N ; i ++) {
+				if(!visited[i] && graph[i][minVertex] != 0 && minEdge[i] > graph[i][minVertex]) {
+					minEdge[i] = graph[i][minVertex];
+				}
+			}
+		}
+		
+		for (int i : minEdge) {
+			result += i;
+		}
+		
+		System.out.println(result);
+		
+	}
+}
