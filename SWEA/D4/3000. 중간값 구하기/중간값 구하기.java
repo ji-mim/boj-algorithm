f7eb84f6beea0b4e1df4fad014c259466f95fc3f
jimin Choi <138632648+ji-mim@users.noreply.github.com>
2026-03-23 13:49:15 +0900

    [D4] Title: 중간값 구하기, Time: 1,024 ms, Memory: 118,968 KB -BaekjoonHub

--- SWEA/D4/3000. 중간값 구하기/중간값 구하기.java
+++ SWEA/D4/3000. 중간값 구하기/중간값 구하기.java
@@ -2,72 +2,64 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	
-	static PriorityQueue<Integer> maxPq = new PriorityQueue<>(Collections.reverseOrder());
-	static PriorityQueue<Integer> minPq = new PriorityQueue<>();
-	static int mod = 20171109;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		
-		for (int tc = 1 ; tc <= T ; tc ++) {
-			maxPq.clear();
-			minPq.clear();
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			
-			int n = Integer.parseInt(st.nextToken());
-			int firstNum = Integer.parseInt(st.nextToken());
-			int result = 0;
-			
-			maxPq.add(firstNum);
-			
-			for (int i = 0 ; i < n ; i++) {
-				st = new StringTokenizer(br.readLine());
-				while(st.hasMoreTokens()) {
-					maxPq.add(Integer.parseInt(st.nextToken()));
-				}
-				
-				// min이 비어있으면 개수 맞춰주기
-				if(minPq.isEmpty()) {
-					share();
-//					System.out.println("max: " + Arrays.toString(maxPq.toArray()));
-//					System.out.println("min: " + Arrays.toString(minPq.toArray()));
-					result += maxPq.peek() % mod;
-					continue;
-				}
-				
-				//max가 min보다 작을 때 까지 옮겨주기 
-				while(!(maxPq.peek() <= minPq.peek())) {
-					minPq.add(maxPq.poll());
-				}
-				
-				share();
-				
-//				System.out.println("max: " + Arrays.toString(maxPq.toArray()));
-//				System.out.println("min: " + Arrays.toString(minPq.toArray()));
-				result += (maxPq.peek()) % mod;
-				result %= mod;
-				
-			}
-			
-			System.out.println("#" + tc + " " + result);
-			
-			
-		}
-		
-	}
-	
-	public static void share() {
-		if(maxPq.size() > minPq.size() + 1) {
-			while(maxPq.size() != minPq.size() + 1) {
-				minPq.add(maxPq.poll());
-			}
-		}else if (maxPq.size() < minPq.size() + 1) {
-			while(maxPq.size() != minPq.size() + 1) {
-				maxPq.add(minPq.poll());
-			}
-		}
-	}
 
+    static final int MOD = 20171109;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int first = Integer.parseInt(st.nextToken());
+
+            PriorityQueue<Integer> maxPq = new PriorityQueue<>(Collections.reverseOrder());
+            PriorityQueue<Integer> minPq = new PriorityQueue<>();
+
+            maxPq.offer(first);
+            long result = 0;
+
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+
+                addNumber(a, maxPq, minPq);
+                addNumber(b, maxPq, minPq);
+
+                result = (result + maxPq.peek()) % MOD;
+            }
+
+            sb.append("#").append(tc).append(" ").append(result).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    static void addNumber(int x, PriorityQueue<Integer> maxPq, PriorityQueue<Integer> minPq) {
+        // 1. 우선 적절한 힙에 넣기
+        if (maxPq.isEmpty() || x <= maxPq.peek()) {
+            maxPq.offer(x);
+        } else {
+            minPq.offer(x);
+        }
+
+        // 2. 크기 균형 맞추기
+        if (maxPq.size() < minPq.size() + 1) {
+            maxPq.offer(minPq.poll());
+        } else if (maxPq.size() > minPq.size() + 1) {
+            minPq.offer(maxPq.poll());
+        }
+
+        // 3. 순서 조건 보정
+        if (!minPq.isEmpty() && maxPq.peek() > minPq.peek()) {
+            int maxTop = maxPq.poll();
+            int minTop = minPq.poll();
+            maxPq.offer(minTop);
+            minPq.offer(maxTop);
+        }
+    }
 }
