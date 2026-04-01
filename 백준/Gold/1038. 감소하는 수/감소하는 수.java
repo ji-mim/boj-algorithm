56ede801b7e9865606688a7d315f338e193e51c4
jimin Choi <138632648+ji-mim@users.noreply.github.com>
2026-04-01 13:34:37 +0900

    [Gold V] Title: 감소하는 수, Time: 176 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.java
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -1,100 +1,95 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
+	
+	static int[][] arr; 
 
-    static int[][] arr; // arr[len][first] = 길이가 len이고 첫 자리가 first인 감소하는 수의 개수
+	
+	public static void main(String args[]) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		arr = new int[12][15]; // r = 자리수, c = 맨 앞자리 수 
+		
+		int[] prefixSum = new int[10];
+		
+		Arrays.fill(arr[1], 1);
+		
+		//arr 채우기 
+		
+		for(int i = 2 ; i <= 11 ; i ++) { // 자릿수 
+			//그 전 자리수 prefixSum 구하기 
+			getPrefixSum(i - 1, prefixSum);
+//			System.out.println(Arrays.toString(prefixSum));
+			for(int j = i - 1 ; j <= 10 ; j ++) {
+				arr[i][j] = prefixSum[j - 1];
+			}
+		}
+		
+//		for (int[] a : arr) {
+//			System.out.println(Arrays.toString(a));
+//		}
+		
+		
+		int[] totalSum = new int[11];
+		totalSum[1] = arr[2][10];
+		for (int i = 2 ; i < 11 ; i ++) {
+			totalSum[i] = totalSum[i - 1] + arr[i + 1][10];
+		}
+		
+//		System.out.println(Arrays.toString(totalSum));
+//		[0, 10, 55, 175, 385, 637, 847, 967, 1012, 1022, 1023]
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+		
+		// N이 1023보다 크면 -1 반환 
+		// N이 10보다 작거나 같으면 그냥 그 값 반환 
+		// 이제 아니면 totalSum 순회하면서 나보다 클 때를 딱 찾아고, 거기를 r로 잡고 arr[r]을 0부터 순회하면서 더하다가 나보다 커지면 거기가 첫번쨰 수, r-1로 올라가서 내 값이 나올 때 까지 더하기 
+		// 이러면  N + 1을 찾아야할 것 같은데 
+		String ans = "";
+		if (N >= 1023) System.out.println(-1);
+		else if (N <= 10) System.out.println(N);
+		else {
+			int findN = N + 1;
+			int len = 0 ;
+			
+			for (int i = 1 ; i < totalSum.length ; i ++) {
+				if (totalSum[i] >= findN) {
+//					ans += String.valueOf(i);
+					findN -= totalSum[i - 1];
+					len = i;
+					break;
+				}
+			}
+			
+			while(len != 0) {
+				int sum = 0;
+				for (int i = 0 ; i < 10 ; i ++) {
+//			        if (i < len - 1) continue;
 
-        // 감소하는 수는 총 1023개 (0 ~ 1022번째)
-        if (N >= 1023) {
-            System.out.println(-1);
-            return;
-        }
-
-        // 0 ~ 9
-        if (N <= 9) {
-            System.out.println(N);
-            return;
-        }
-
-        arr = new int[11][10]; // 길이 1~10, 첫 자리 0~9
-        int[] prefixSum = new int[10];
-
-        // 길이 1인 감소하는 수
-        for (int i = 0; i <= 9; i++) {
-            arr[1][i] = 1;
-        }
-
-        // DP 채우기
-        for (int len = 2; len <= 10; len++) {
-            getPrefixSum(len - 1, prefixSum);
-
-            for (int first = 0; first <= 9; first++) {
-                // 길이가 len이면 첫 자리는 적어도 len-1 이상이어야 함
-                // 예: 길이 3이면 첫 자리는 최소 2
-                if (first >= len - 1) {
-                    arr[len][first] = prefixSum[first - 1];
-                }
-            }
-        }
-
-        // totalSum[len] = 1자리 ~ len자리 감소하는 수의 누적 개수
-        int[] totalSum = new int[11];
-        totalSum[1] = 10; // 0~9
-
-        for (int len = 2; len <= 10; len++) {
-            int cnt = 0;
-            for (int first = 0; first <= 9; first++) {
-                cnt += arr[len][first];
-            }
-            totalSum[len] = totalSum[len - 1] + cnt;
-        }
-
-        // N번째 수가 몇 자리인지 찾기
-        int len = 1;
-        while (totalSum[len] <= N) {
-            len++;
-        }
-
-        // len자리 감소수들 중 몇 번째인지 (1-based)
-        int k = N - totalSum[len - 1] + 1;
-
-        StringBuilder sb = new StringBuilder();
-        int maxDigit = 9;
-
-        while (len > 0) {
-            for (int digit = 0; digit <= maxDigit; digit++) {
-                // 남은 len자리 수를 만들려면 현재 digit은 최소 len-1 이상이어야 함
-                if (digit < len - 1) continue;
-
-                int cnt;
-                if (len == 1) {
-                    cnt = 1;
-                } else {
-                    cnt = arr[len][digit];
-                }
-
-                if (k > cnt) {
-                    k -= cnt;
-                } else {
-                    sb.append(digit);
-                    maxDigit = digit - 1;
-                    len--;
-                    break;
-                }
-            }
-        }
-
-        System.out.println(sb.toString());
-    }
-
-    static void getPrefixSum(int r, int[] prefix) {
-        prefix[0] = arr[r][0];
-        for (int i = 1; i <= 9; i++) {
-            prefix[i] = prefix[i - 1] + arr[r][i];
-        }
-    }
+					sum += arr[len][i];
+					
+					if(sum >= findN) {
+						ans += String.valueOf(i);
+						findN -= (sum - arr[len][i]);
+						len --;
+						break;
+					}
+				}
+			}
+			
+			System.out.print(ans);
+		}
+	}
+	
+	
+	static void getPrefixSum(int r, int[] prefix) {
+		
+		prefix[r-1] = arr[r][r-1];
+		
+		for (int i = r ; i <= 9 ; i ++) {
+			prefix[i] = prefix[i-1] + arr[r][i];
+		}
+	}
 }
